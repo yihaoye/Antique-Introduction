0eefa15f0c18c1bc94cfa095bebebaa89b61ce2d
yihaoye <yihaoyemail@gmail.com>
2017-03-08 22:52:57 +1300

    react-bootstrap rebuild project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
